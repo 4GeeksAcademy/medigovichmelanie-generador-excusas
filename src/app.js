3b86ffd27af52bcad24ccc90c445dfc05b8b3467
Melanie Dana Medigovich <melacem@gmail.com>
2025-03-11 19:37:57 +0000

    proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateExcuse(); // Generar una excusa cuando la página cargue
+  // Agregar funcionalidad para generar una nueva excusa con un botón
+  document.getElementById("generateButton").addEventListener("click", generateExcuse);
 };
+
+function generateExcuse() {
+  let quien = ["Mi abuela", "Mi hermana", "Mi coche", "Mi perro"];
+  let accion = ["se accidentó", "se enfermó", "se averió", "se perdió"];
+  let cuando = ["ayer", "anoche", "esta mañana", "hace una hora"];
+  let que = ["y debo llevarla al hospital", "y debo cuidarla", "y debo llevarlo al taller", "y debo encontrarlo"];
+  let excuse =
+    `${quien[Math.floor(Math.random() * quien.length)]}
+    ${accion[Math.floor(Math.random() * accion.length)]}
+    ${cuando[Math.floor(Math.random() * cuando.length)]}
+    ${que[Math.floor(Math.random() * que.length)]}.`;
+  document.getElementById("excuse").innerText = excuse; // Mostrar en el HTML
+}
